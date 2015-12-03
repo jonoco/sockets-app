2b2936a2875a967a15c70cbb09b5af018d69d54c
Joshua Cox <joshua.n.cox@gmail.com>
2015-12-02 20:11:55 -0800

    show timestamps on ui

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,16 @@
 var socket = io();
+var now = moment();
 
 socket.on('connect', function() {
 	console.log('Connected to socket.io server');
 });
 
 socket.on('message', function(message) {
-	console.log(message.text);
-	$('.messages').append('<p>'+message.text+'</p>');
+	console.log(message.text, message.timestamp);
+
+	var timestamp = message.timestamp ? moment(message.timestamp).local().format('h:mm') : '';
+
+	$('.messages').append('<p>'+message.text+'<span class="timestamp">'+timestamp+'</span>'+'</p>');
 });
 
 // handles submitting new message
@@ -15,9 +19,11 @@ $form.on('submit', function(event) {
 	event.preventDefault();
 
 	var $message = $form.find('input[name=message]');
+	var timestamp = moment().valueOf();
 
 	socket.emit('message', {
-		text: $message.val()
+		text: $message.val(),
+		timestamp: timestamp
 	});
 
 	$message.val('');
