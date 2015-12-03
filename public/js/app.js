bf51db27dcf24f11d29797bdf9f8c478754cbd53
Joshua Cox <joshua.n.cox@gmail.com>
2015-12-03 14:35:40 -0800

    added support for chat rooms

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,31 @@
 var socket = io();
 var now = moment();
 
+var room = getQueryVariable('room') || 'Anonymous';
+var name = getQueryVariable('name');
+console.log(room, name);
+
+// set room name
+$('.room-title').text(room);
+ 
 socket.on('connect', function() {
 	console.log('Connected to socket.io server');
+
+	socket.emit('joinRoom', {
+		name: name,
+		room: room
+	});
 });
 
 socket.on('message', function(message) {
 	console.log(message.text, message.timestamp);
+	var $messages = $('.messages');
+	var timestamp = message.timestamp ? moment(message.timestamp).local().format('h:mma') : '';
 
-	var timestamp = message.timestamp ? moment(message.timestamp).local().format('h:mm') : '';
-
-	$('.messages').append('<p>'+message.text+'<span class="timestamp">'+timestamp+'</span>'+'</p>');
+	var messageName = '<span class="name">'+message.name+'</span>';
+	var messageTime = '<span class="time">'+timestamp+'</span>'; 
+	var messageText = '<p class="text">'+message.text+'</p>';
+	$messages.append('<div class="message">'+messageName+messageTime+messageText+'</div>');
 });
 
 // handles submitting new message
@@ -22,6 +37,7 @@ $form.on('submit', function(event) {
 	var timestamp = moment().valueOf();
 
 	socket.emit('message', {
+		name: name,
 		text: $message.val(),
 		timestamp: timestamp
 	});
--- server.js
+++ server.js
@@ -7,17 +7,32 @@ var moment = require('moment');
 
 app.use(express.static(__dirname + '/public'));
 
+var clientInfo = {};
+
 io.on('connection', function(socket) {
 	console.log('User conntected via socket.io');
+
+	socket.on('joinRoom', function(req) {
+		clientInfo[socket.id] = req;
+		// socket.io handles joining functionality
+		socket.join(req.room);
+		socket.broadcast.to(req.room).emit('message', {
+			name: 'System',
+			text: req.name + ' has joined',
+			timestamp: moment().valueOf()
+		})
+	});
 
 	socket.on('message', function(message) {
 		console.log('Message received: ' + message.text );
 
-		io.emit('message', message);
+		io.to(clientInfo[socket.id].room).emit('message', message);
 	});
 
 	socket.emit('message', {
-		text: 'Welcome to the room'
+		name: 'System',
+		text: 'Welcome to the room',
+		timestamp: moment().valueOf()
 	});
 });
 
